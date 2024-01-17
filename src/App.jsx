4ab9b01ff09341c05b09f429c5bc5a7e066a55de
Allison Sequeira <sequeira416@gmail.com>
2024-01-17 09:29:17 -0600

    challenge week 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,72 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([])
+
+  useEffect(() =>{
+    fetch('https://ghibliapi.dev/films')
+    .then((res) => res.json())
+    .then((res) => {
+      console.log(res)
+      const sgMovies = Object.values(res)
+      setMovies(sgMovies)
+
+    })
+  }, [])
+
+  const [time, setTime] = useState(new Date())
+  useEffect(() =>{
+    setInterval(() =>{
+      setTime(new Date())
+    }, 1000)
+  }, [])
+
+  useEffect(() => {
+    function getScroll(){
+      const currentScroll = document.documentElement.scrollTop;
+      console.log(currentScroll)
+    }
+
+    document.addEventListener('scroll', getScroll)
+  })
+
+  function handleChange(event){
+    setCount(Number(event.target.value));
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Week 2</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          +1
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <input onChange={handleChange} value={count}></input>
+        <button onClick={() => setCount((count) => count - 1)}>
+          -1
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+      <button onClick={() => setCount((count) => count + 10)}>
+          +10
+        </button>
+        <button onClick={() => setCount((count) => count - 10)}>
+          -10
+        </button>
+      </div>
+
+      <h2>Studio Ghibli Movies</h2>
+      <ul>
+        {movies.map(movie => (
+          <li>{movie.title} {movie.release_date}</li>
+        ))}
+      </ul>
+      <h2>Time</h2>
+      <time>{time.toTimeString()}</time>
     </>
   )
 }
